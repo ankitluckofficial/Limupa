637232269f687f9c20c7609c215c196ba9526337
Ankit Gupta <ankitgupta81129@gmail.com>
2026-09-01 14:36:48 +0530

    change

--- app/Http/Controllers/Front/AuthController.php
+++ app/Http/Controllers/Front/AuthController.php
@@ -8,29 +8,18 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-  public function showLoginForm() {
+    public function showLoginForm()
+    {
         return view('auth.login');
     }
-     // Show register form
-    public function showRegisterForm() {
+    // Show register form
+    public function showRegisterForm()
+    {
         return view('auth.register');
     }
 
-    // public function login(Request $request) {
-    //     $credentials = $request->only('email', 'password');
-    //     $remember = $request->filled('remember'); // checkbox check
-
-    //     if (Auth::attempt($credentials, $remember)) {
-    //         // ✅ Redirect to home page after login
-    //         return redirect()->intended('/');
-    //     }
-
-    //     return back()->withErrors([
-    //         'email' => 'Invalid credentials.',
-    //     ]);
-    // }
-
-    public function logout() {
+    public function logout()
+    {
         Auth::logout();
         return redirect('/login');
     }
